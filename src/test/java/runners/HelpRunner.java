ceb6ea36ce89c2dd1c8be51f020cec7704f24847
Hany Khalil <hany.khalil@expleogroup.com>
2023-01-28 15:13:29 +0100

    new cucumber framwork

---
+++ src/test/java/runners/HelpRunner.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+package runners;
+
+/**
+ * @author 13465
+ *
+ */
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "C:\\Users\\13465\\eclipse\\workspace\\cucumber\\src\\test\\java\\cucumber\\features"
+, glue = "stepDefintion", tags = "@regression", plugin = {"pretty", "html:target/reports/cucumberReport.html", "json:target/reports/cucumberReport.json"}, monochrome = true)
+public class HelpRunner {
+
+}
+
---
+++ src/test/java/stepDefintion/Hooks.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+package stepDefintion;
+import cucumber.mainCucumber.TestBase;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+/**
+ * @author 13465
+ *
+ */
+public class Hooks extends TestBase{
+
+	@Before("@regression")
+	public void runBeforeAnyScenario()
+	{
+
+	}
+
+	@After("@regression")
+	public void runAfterAnyScenario()
+	{
+		driver.quit();
+	}
+}
+
