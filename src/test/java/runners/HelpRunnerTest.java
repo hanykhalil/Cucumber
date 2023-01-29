74bad1b53ee41d00da3f97864bbcf2225514a5e5
Hany Khalil <hany.khalil@expleogroup.com>
2023-01-29 04:46:22 +0100

    new1

--- src/test/java/runners/HelpRunnerTest.java
+++ src/test/java/runners/HelpRunnerTest.java
@@ -14,7 +14,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\Cucumber\\src\\test\\java\\features"
+@CucumberOptions(features = "C:\\Users\\13465\\eclipse\\workspace\\cucumber\\src\\test\\java\\cucumber\\features"
 , glue = "stepDefintion", tags = "@regression", plugin = {"pretty", "html:target/reports/cucumberReport.html", "json:target/reports/cucumberReport.json"}, monochrome = true)
 public class HelpRunnerTest {
 
